f173c515eef372242692152dfb005eebbd6ee5f6
yunlong <349558264@qq.com>
2019-06-22 18:48:09 +0800

    [yunlong/yunlong] edit result

--- src/main/java/com/stu/edu/spike/result/Result.java
+++ src/main/java/com/stu/edu/spike/result/Result.java
@@ -38,23 +38,14 @@ public class Result <T>{
         return code;
     }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
 
     public String getMsg() {
         return msg;
     }
 
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
 
     public T getData() {
         return data;
     }
 
-    public void setData(T data) {
-        this.data = data;
-    }
 }
