3a61bad490c80aceef5ce1b5727794e728301a65
yunlong <349558264@qq.com>
2019-06-22 16:27:00 +0800

    [yunlong/yunlong] add global result

--- src/main/java/com/stu/edu/spike/result/CodeMsg.java
+++ src/main/java/com/stu/edu/spike/result/CodeMsg.java
@@ -1,4 +1,35 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package com.stu.edu.spike.result;
 
 public class CodeMsg {
+    private int code;
+    private String msg;
+
+    public CodeMsg(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
--- src/main/java/com/stu/edu/spike/result/Result.java
+++ src/main/java/com/stu/edu/spike/result/Result.java
@@ -1,4 +1,60 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package com.stu.edu.spike.result;
 
-public class Result {
+public class Result <T>{
+    private int code;
+    private String msg;
+    private T data;
+
+    public static <T> Result<T> success(T data){
+        return new Result<T>(data);
+    }
+    public static <T> Result<T> fail(CodeMsg msg){
+        return new Result<T>(msg);
+    }
+
+    private Result(CodeMsg msg){
+        if (msg == null){
+            return;
+        }
+        this.code = msg.getCode();
+        this.msg = msg.getMsg();
+    }
+
+    private Result(T data){
+        this.code = 0;
+        this.msg = "success";
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
